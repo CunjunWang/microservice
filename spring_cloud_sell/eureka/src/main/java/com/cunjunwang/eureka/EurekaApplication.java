a06e3083a2ea70313fa2f7cf38107e26dce59c7f
Cunjun Wang <13621691063@163.com>
2018-08-09 23:24:56 +0800

    eureka server

--- spring_cloud_sell/eureka/src/main/java/com/cunjunwang/eureka/EurekaApplication.java
+++ spring_cloud_sell/eureka/src/main/java/com/cunjunwang/eureka/EurekaApplication.java
@@ -2,8 +2,11 @@ package com.cunjunwang.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+// 表示项目有注册中心功能
+@EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
