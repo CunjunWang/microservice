1e9716aecfe8183b6374492ebea662d63e21d288
Cunjun Wang <13621691063@163.com>
2018-08-17 11:23:43 +0800

    added zuul user auth, added user model

--- spring_cloud_sell/eureka/src/main/java/com/cunjunwang/eureka/EurekaApplication.java
+++ spring_cloud_sell/eureka/src/main/java/com/cunjunwang/eureka/EurekaApplication.java
@@ -6,8 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 // 表示项目有注册中心功能
-// Eureka服务端 让服务注册的服务
-// 器
+// Eureka服务端 让服务注册的服务器
 @EnableEurekaServer
 public class EurekaApplication {
 
