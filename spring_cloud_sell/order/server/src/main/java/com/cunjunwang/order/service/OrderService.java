3becc8aa7a42a74f5de53f91bf9f49768c80a9f0
Cunjun Wang <13621691063@163.com>
2018-08-20 18:41:22 +0800

    api-gateway zuul added

--- spring_cloud_sell/order/server/src/main/java/com/cunjunwang/order/service/OrderService.java
+++ spring_cloud_sell/order/server/src/main/java/com/cunjunwang/order/service/OrderService.java
@@ -4,5 +4,17 @@ import com.cunjunwang.order.dto.OrderDTO;
 
 public interface OrderService {
 
+    /**
+     * 创建订单
+     * @param orderDTO
+     * @return
+     */
     OrderDTO create(OrderDTO orderDTO);
+
+    /**
+     * 完结订单(只能卖家操作)
+     * @param orderId
+     * @return
+     */
+    OrderDTO finish(String orderId);
 }
--- spring_cloud_sell/order/server/src/main/java/com/cunjunwang/order/service/impl/OrderServiceImpl.java
+++ spring_cloud_sell/order/server/src/main/java/com/cunjunwang/order/service/impl/OrderServiceImpl.java
@@ -5,6 +5,8 @@ import com.cunjunwang.order.dataobject.OrderMaster;
 import com.cunjunwang.order.dto.OrderDTO;
 import com.cunjunwang.order.enums.OrderStatusEnum;
 import com.cunjunwang.order.enums.PaymentStatusEnum;
+import com.cunjunwang.order.enums.ResultEnum;
+import com.cunjunwang.order.exceptions.OrderException;
 import com.cunjunwang.order.repository.OrderDetailRepository;
 import com.cunjunwang.order.repository.OrderMasterRepository;
 import com.cunjunwang.order.service.OrderService;
@@ -15,16 +17,14 @@ import com.cunjunwang.product.common.ProductInfoOutput;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
-
-//import com.cunjunwang.product.client.ProductClient;
-//import com.cunjunwang.product.common.DecreaseStockInput;
-//import com.cunjunwang.product.common.ProductInfoOutput;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -51,11 +51,17 @@ public class OrderServiceImpl implements OrderService {
 
         // 计算总价
 
+        // 异步扣库存思路：
+        // 读redis 并判断库存是否足够
+        // 减库存并把新值重新设置进redis
+        // 需要加redis锁考虑高并发的情况
+        // 订单入库异常需要手动回滚redis
+
         BigDecimal orderAmount = new BigDecimal(BigInteger.ZERO);
         for (OrderDetail orderDetail : orderDTO.getOrderDetailList()) {
 
-            for(ProductInfoOutput productInfoOutput : productInfoOutputList) {
-                if(productInfoOutput.getProductId().equals(orderDetail.getProductId())) {
+            for (ProductInfoOutput productInfoOutput : productInfoOutputList) {
+                if (productInfoOutput.getProductId().equals(orderDetail.getProductId())) {
                     orderAmount = productInfoOutput.getProductPrice()
                             .multiply(new BigDecimal(orderDetail.getProductQuantity()))
                             .add(orderAmount);
@@ -87,4 +93,39 @@ public class OrderServiceImpl implements OrderService {
         orderMasterRepository.save(orderMaster);
         return orderDTO;
     }
+
+    @Override
+    @Transactional
+    public OrderDTO finish(String orderId) {
+        // 1. 查询订单
+        Optional<OrderMaster> orderMasterOptional = orderMasterRepository.findById(orderId);
+        if (!orderMasterOptional.isPresent()) {
+            throw new OrderException(ResultEnum.ORDER_NOT_EXIST);
+        }
+
+        // 2. 判断订单状态
+        OrderMaster orderMaster = orderMasterOptional.get();
+        if (!OrderStatusEnum.NEW.getCode().equals(orderMaster.getOrderStatus())) {
+            throw new OrderException(ResultEnum.ORDER_STATUS_ERROR);
+        }
+
+        // 3. 修改状态为完结
+        orderMaster.setOrderStatus(OrderStatusEnum.FINISHED.getCode());
+        orderMasterRepository.save(orderMaster);
+
+        // 查询订单详情
+        List<OrderDetail> orderDetailList = orderDetailRepository.findByOrderId(orderId);
+
+        if (CollectionUtils.isEmpty(orderDetailList)) {
+            throw new OrderException(ResultEnum.ORDER_DETAIL_NOT_EXIST);
+        }
+
+        OrderDTO orderDTO = new OrderDTO();
+        BeanUtils.copyProperties(orderMaster, orderDTO);
+        orderDTO.setOrderDetailList(orderDetailList);
+
+        return orderDTO;
+    }
+
+
 }
